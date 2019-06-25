76ccdeff4ce6e6361c97743b794b3fef469828f3
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2019-06-24 21:53:13 -0700

    Simpler, I guess and precomputed array.

--- jun23/jun23.ino
+++ jun23/jun23.ino
@@ -4,7 +4,7 @@
 #define NUMPIXELS 90
 
 // How many strips are present
-#define NUMSTRIPS 5
+#define NUMSTRIPS 1
 
 // First output pin. Strips should be at pins STARTPIN to STARTPIN+NUMSTRIPS-1.
 #define STARTPIN 6
@@ -12,60 +12,92 @@
 // Time (in milliseconds) to pause between steps
 #define DELAYVAL 0
 
-Adafruit_NeoPixel pixels[NUMSTRIPS];
+// How long is the signal in pixels. Pixels in front of the signal and behind
+// will be set to zero.
+#define SIGNAL_LENGTH 30
+// How many steps (in pixels) to take.
+#define NUM_STEPS 300
+// Global multiplier for brightness. In range [0, 1.0].
+#define BRIGHTNESS 1.0
 
-enum class WaveType {
-  kSin = 0,
+enum class SignalType {
+  kMinusExp = 0,
+  kSin = 1
 };
 
-void getRgb(const WaveType wave_type, const int step, const int num_steps,
-            const int led_idx, const int num_leds, uint8_t* r, uint8_t* g,
-            uint8_t* b) {
-  const float idx_normalized = static_cast<float>(led_idx) / num_leds;
-  const float step_normalized = static_cast<float>(step) / num_steps;
-  float val = 0.0;
-  const int diff = step - led_idx;
-  if (diff > 0 && diff < 20) {
-    val = exp(-static_cast<float>((abs(diff))) / 2);
+void getSignal(const SignalType type, const int t, uint8_t* r, uint8_t* g, uint8_t* b, uint8_t* w) {
+  const float v = static_cast<float>(t) / SIGNAL_LENGTH;
+  if (type == SignalType::kMinusExp) {
+    const float kFadeMultiplier = 7.0;
+    *r = static_cast<uint8_t>(exp(-v * kFadeMultiplier) * 255.0 * BRIGHTNESS);
+    *g = *r;
+    *b = *r;
+    *w = *r;
+  } else if (type == SignalType::kSin) {
+    *r = static_cast<uint8_t>(sin(v * PI) * 255.0 * BRIGHTNESS);
+    *g = *r;
+    *b = *r;
+    *w = *r;
+  } else {
+    *r = 0;
+    *g = 0;
+    *b = 0;
+    *w = 0;
   }
-  //const float val = sin((idx_normalized + step_normalized) * PI);
-  float normalized_val = max(0.0, min(1.0, val));
-  *r = static_cast<uint8_t>(normalized_val * 255);
-  *g = static_cast<uint8_t>(normalized_val * 255);
-  *b = static_cast<uint8_t>(normalized_val * 255);
 }
 
+Adafruit_NeoPixel pixels[NUMSTRIPS];
+
+// Precomputed arrays.
+uint8_t r_arr[SIGNAL_LENGTH];
+uint8_t g_arr[SIGNAL_LENGTH];
+uint8_t b_arr[SIGNAL_LENGTH];
+uint8_t w_arr[SIGNAL_LENGTH];
+
+const SignalType type = SignalType::kMinusExp;
 void setup() {
   for (int i = 0; i < NUMSTRIPS; ++i) {
     pixels[i] = Adafruit_NeoPixel(NUMPIXELS, STARTPIN+i, NEO_RGBW + NEO_KHZ800);
     pixels[i].begin();
   }
-}
-
-void loop() {
   // Set all pixel colors to 'off'
   for (int i = 0; i < NUMSTRIPS; ++i) {
     pixels[i].clear();
   }
-
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
-  const int kNumSteps = 100;
-  uint8_t r, g, b;
-  int step = 0;
-
-  while (true) {
-    step = step % kNumSteps;
-    for (int led = 0; led < NUMPIXELS; ++led) {
-      getRgb(WaveType::kSin, step, kNumSteps, led, NUMPIXELS, &r, &g, &b);
-      for (int i = 0; i < NUMSTRIPS; ++i) {
-        pixels[i].setPixelColor(led, pixels[i].Color(g, r, b));
-      }
-    }
-    for (int i = 0; i < NUMSTRIPS; ++i) {
-      pixels[i].show();
-    }
-    ++step;
-    delay(DELAYVAL);
+  // Precompute arrays.
+  uint8_t r, g, b, w;
+  for (int i = 0; i < SIGNAL_LENGTH; ++i) {
+    getSignal(type, i, &r, &g, &b, &w);
+    r_arr[i] = r;
+    g_arr[i] = g;
+    b_arr[i] = b;
+    w_arr[i] = w;
   }
+}
+
+int step = 0;
+void loop() {
+  uint8_t r, g, b, w;
+  step = step % NUM_STEPS;
+  for (int strip = 0; strip < NUMSTRIPS; ++strip) {
+    for (int led = 0; led < NUMPIXELS; ++led) {
+      const int idx = step - led;
+      r = 0;
+      g = 0;
+      b = 0;
+      w = 0;
+      if (idx < SIGNAL_LENGTH && idx > 0) {
+        r = r_arr[idx];
+        g = g_arr[idx];
+        b = b_arr[idx];
+        w = w_arr[idx];
+      }
+      pixels[strip].setPixelColor(led, r, g, b, w);
+    }
+  }
+  for (int i = 0; i < NUMSTRIPS; ++i) {
+    pixels[i].show();
+  }
+  ++step;
+  delay(DELAYVAL);
 }
