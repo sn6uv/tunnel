010bbb2ab7ac71946071986b0db78d7f785f9e2b
Juhana Kangaspunta <juhana@juhana-macbookpro.roam.corp.google.com>
2019-08-22 23:47:02 -0700

    Ready for the playa.

--- jun23/jun23.ino
+++ jun23/jun23.ino
@@ -10,14 +10,17 @@ constexpr int kNumStrips = 5;
 // First output pin. Strips should be at pins kStartPin to kStartPin+kNumStrips-1.
 constexpr int kStartPin = 8;
 
+// Which pin the button is connected to.
 constexpr int kButtonPin = 2;
 
 // Global multiplier for brightness. In range [0, 255].
 constexpr uint8_t kBrightness = 255;
 
+// Handles button state.
 class Button {
  public:
-  bool didChange() {
+  // Returns true if button was pressed after the last call.
+  bool wasPressed() {
     int reading = digitalRead(kButtonPin);
     bool did_change = false;
     if (reading != last_button_state_) {
@@ -41,6 +44,7 @@ class Button {
   int last_button_state_ = HIGH;
   unsigned long last_debounce_time_ = 0;
 };
+
 
 struct State {
   Adafruit_NeoPixel pixels[kNumStrips];
@@ -73,11 +77,33 @@ class Confetti : public Effect {
     fadeToBlackBy(state.vals, 7);
 
     for (int strip = 0; strip < kNumStrips; ++strip) {
-      int pos = random16(kNumPixels);
+
+      auto& strip_obj = state.pixels[strip];
+
+      int pos = random(kNumPixels);
       state.vals[strip][pos] = 255;
-      state.hues[strip][pos] = state.hue + random8(64);
+      state.hues[strip][pos] = state.hue + random(64);
       for (int led = 0; led < kNumPixels; ++led) {
-        state.pixels[strip].setPixelColor(led, state.pixels[strip].ColorHSV(static_cast<uint16_t>(state.hues[strip][led]) * 256, 200, state.vals[strip][led])); 
+        strip_obj.setPixelColor(led, strip_obj.gamma32(strip_obj.ColorHSV(static_cast<uint16_t>(state.hues[strip][led]) * 256, 150, state.vals[strip][led])));
+      }
+    }
+  }
+};
+
+class Angus : public Effect {
+ public:
+  void step(State& state) override {
+    static long int t = 0;
+    t++;
+    constexpr float kFreq = 0.5;
+    constexpr float kDelta = 0.1;
+
+    for (int strip = 0; strip < kNumStrips; ++strip) {
+      auto& strip_obj = state.pixels[strip];
+      for (int led = 0; led < kNumPixels; ++led) {
+        float phase = kDelta * led;
+        float val = (sin(kFreq * t - phase) + 1.0) / 2.0;
+        strip_obj.setPixelColor(led, strip_obj.gamma32(strip_obj.ColorHSV(state.hue * 255, 255, val * 255)));
       }
     }
   }
@@ -93,9 +119,10 @@ class Sinelon : public Effect {
       int pos = beatsin16(20, 0, kNumPixels - 1);
       
       for (int strip = 0; strip < kNumStrips; ++strip) {
+        auto& strip_obj = state.pixels[strip];
         state.vals[strip][pos] = 255;
         for (int led = 0; led < kNumPixels; ++led) {
-          state.pixels[strip].setPixelColor(led, state.pixels[strip].ColorHSV(state.hue * 256, 255, state.vals[strip][led]));
+          strip_obj.setPixelColor(led, strip_obj.gamma32(strip_obj.ColorHSV(65535, 0, state.vals[strip][led])));
         }
       }
     }
@@ -104,19 +131,20 @@ class Sinelon : public Effect {
 class MovingPulse : public Effect {
  public:
   void step(State& state) override {
-    fadeToBlackBy(state.vals, 40);
+    fadeToBlackBy(state.vals, 20);
 
-    constexpr int kBpm = 60;
-    constexpr long kTail = 90;
-    const float progress = beat8(kBpm) / 255.0f;
-    const int pos = progress * (kNumPixels + kTail);
+    constexpr int kBpm = 40;
+    constexpr long kTail = 30;
+    const float progress = beat16(kBpm) / static_cast<float>(65536l);
+    const long int pos = progress * (kNumPixels + kTail);
     
     for (int strip = 0; strip < kNumStrips; ++strip) {
+      auto& strip_obj = state.pixels[strip];
       if (pos < kNumPixels) {
         state.vals[strip][pos] = 255;
       }
       for (int led = 0; led < kNumPixels; ++led) {
-        state.pixels[strip].setPixelColor(led, state.pixels[strip].ColorHSV(state.hue * 256, 255, state.vals[strip][led]));
+        strip_obj.setPixelColor(led, strip_obj.gamma32(strip_obj.ColorHSV(65536, 0, state.vals[strip][led])));
       }
     }
   }
@@ -124,31 +152,44 @@ class MovingPulse : public Effect {
 
 class Rainbow : public Effect {
  public:
-  Rainbow(const bool random_offsets = false) {
-    for (int i = 0; i < kNumStrips; ++i) {
-      long offset = 0;
-      if (random_offsets) {
-        offset = random(kColorWheelRange_);
-      }
-      steps_[i] = offset;
-    }
-  }
   void step(State& state) override {
+    
+    constexpr int kBpm = 40;
+    uint16_t start_hue = beat16(kBpm);
+    constexpr int kRainbowLengthPixels = 60;
+
     for (int strip = 0; strip < kNumStrips; ++strip) {
-      steps_[strip] = steps_[strip] % kColorWheelRange_;
-      for (int led = 0; led < kNumPixels; ++led) {
-        state.hues[strip][led] = steps_[strip] + (led * 256 / kNumPixels);        
-        state.pixels[strip].setPixelColor(led, state.pixels[strip].ColorHSV(state.hues[strip][led] * 256));
+      auto& strip_obj = state.pixels[strip];
+      for (int led = 0; led < kNumPixels; ++led) {        
+        long int hue = (start_hue - 65535l * led  / kRainbowLengthPixels) % 65536l;
+        strip_obj.setPixelColor(led, strip_obj.gamma32(strip_obj.ColorHSV(hue, 255, 255)));
       }
-      steps_[strip] += 7;
     }
   }
- private:
-  static constexpr long kColorWheelRange_ = 256;
-  static constexpr int kDelay_= 0;
-  long steps_[kNumStrips];
 };
 
+class RainbowPulses : public Effect {
+ public:
+
+  void step(State& state) override {
+    constexpr int kBpm = 50;
+    long start_hue = beat16(kBpm);
+    constexpr int kRainbowLengthPixels = 30;
+
+    constexpr long kTail = 10000;
+    for (int strip = 0; strip < kNumStrips; ++strip) {
+      for (int led = 0; led < kNumPixels; ++led) {        
+        uint16_t hue = (start_hue - 65535l * static_cast<long>(led)  / kRainbowLengthPixels) % 65536l;
+        uint8_t val = 255;
+        if (hue > 65536l - kTail) {
+          val = 0;
+        }
+        hue = (65536l * hue) / (65536l - kTail);
+        state.pixels[strip].setPixelColor(led, state.pixels[strip].ColorHSV(hue, 255, val));
+      }
+    }
+  }
+};
 
 State global_state;
 
@@ -156,17 +197,18 @@ Rainbow rainbow;
 MovingPulse pulse;
 Sinelon sinelon;
 Confetti confetti;
+RainbowPulses rainbow_pulse;
+Angus angus;
 
-Effect* effects[] = {&confetti, &sinelon, &rainbow, &pulse};
+Effect* effects[] = {&confetti, &rainbow, &angus, &pulse};
 
 void setup() {
   
   for (int i = 0; i < kNumStrips; ++i) {
-    global_state.pixels[i] = Adafruit_NeoPixel(kNumPixels, kStartPin+i, NEO_RGBW + NEO_KHZ800);
+    global_state.pixels[i] = Adafruit_NeoPixel(kNumPixels, kStartPin + i, NEO_RGBW + NEO_KHZ800);
     global_state.pixels[i].begin();
     global_state.pixels[i].clear();
     global_state.pixels[i].setBrightness(kBrightness);
-    global_state.pixels[i].show();
 
     for (int j = 0; j < kNumPixels; ++j) {
       global_state.hues[i][j] = 0;
@@ -177,14 +219,15 @@ void setup() {
 }
 
 void loop() {
-  if (global_state.button.didChange()) {
-    global_state.current_effect = (global_state.current_effect + 1) % (sizeof(effects) / sizeof(Effect*));
+  constexpr int kNumEffects = sizeof(effects) / sizeof(Effect*);
+  if (global_state.button.wasPressed()) {
+    global_state.current_effect = (global_state.current_effect + 1) % kNumEffects;
   }
 
   effects[global_state.current_effect]->step(global_state);
   for (int i = 0; i < kNumStrips; ++i) {
     global_state.pixels[i].show();
   }
-  EVERY_N_MILLISECONDS( 20 ) { global_state.hue++; }
+  EVERY_N_MILLISECONDS( 40 ) { global_state.hue++; }
 
 }
